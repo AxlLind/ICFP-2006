574a2295619d62abb0f3a75802f58142be5607b7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-05-13 08:48:02 +0200

    Main: Dont assume input file path

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn write_buf(buf: &mut Vec<u8>) -> Result<()> {
   Ok(())
 }
 
-fn run_cpu(cpu: &mut CPU) -> Result<()> {
+fn run_cpu(mut cpu: CPU) -> Result<()> {
   let mut input = InputReader::new();
   let mut buf = Vec::new();
   loop {
@@ -42,37 +42,38 @@ fn run_cpu(cpu: &mut CPU) -> Result<()> {
   Ok(())
 }
 
+fn sandmark() -> Result<()> {
+  let program = read_program("files/sandmark.umz")?;
+  let cpu = CPU::new(&program);
+  run_cpu(cpu)
+}
+
 fn codex() -> Result<()> {
   let program = read_program("files/codex.umz")?;
   let mut cpu = CPU::new(&program);
   cpu.push_str("(\\b.bb)(\\v.vv)06FHPVboundvarHRAk\np");
-  for _ in 0..195 { cpu.execute(); }
-  run_cpu(&mut cpu)
-}
-
-fn sandmark() -> Result<()> {
-  let program = read_program("files/sandmark.umz")?;
-  let mut cpu = CPU::new(&program);
-  run_cpu(&mut cpu)
+  for _ in 0..195 { cpu.execute(); } // skip text before codex
+  run_cpu(cpu)
 }
 
 fn umix(args: &[String]) -> Result<()> {
   let program = read_program("files/umix.umz")?;
   let mut cpu = CPU::new(&program);
-  if let Some(s) = args.get(2) {
-    let path = format!("inputs/{}.txt", s);
+  if let Some(path) = args.get(2) {
     let input = std::fs::read_to_string(path)?;
     cpu.push_str(input.trim());
   }
-  run_cpu(&mut cpu)
+  run_cpu(cpu)
 }
 
 fn main() -> Result<()> {
   let args = std::env::args().collect::<Vec<_>>();
-  if args.len() < 2 { return umix(&args); }
-  match &args[1][..] {
+  let default_mode = "umix".to_string();
+  let mode = args.get(1).unwrap_or(&default_mode);
+  match &mode[..] {
     "sandmark" => sandmark(),
     "codex"    => codex(),
-    _          => umix(&args),
+    "umix"     => umix(&args),
+    _ => panic!("invalid program"),
   }
 }
